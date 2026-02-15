6a95e4f1c37b1ba061e5ed3073ca693d48bc2c0a
Leila Cielok <leila.cielok@studenti.unimi.it>
2026-02-15 16:35:50 +0100

    don't need fast tuning anymore

--- rockpaperscissors/config.py
+++ rockpaperscissors/config.py
@@ -7,7 +7,7 @@ CLASSES = ["rock", "paper", "scissors"]
 EVAL_RESIZE_MODE = "pad"     # letterboxing 
 EVAL_RECALIB = "uniform"     
 RECALIB_ALPHA = 1.0          
-EVAL_ZERO_BIAS = True        # rimuove il prior appreso nell’ultimo Dense
+EVAL_ZERO_BIAS = True  
 EVAL_TTA = True
 EVAL_TTA_ROT = True          # ±12°
 EVAL_OUTROOT = "reports/custom_eval_myhands" 
@@ -17,7 +17,6 @@ EVAL_ALWAYS_SUBDIR = False
 
 TUNING = True
 TUNING_MODELS = ["c"] # choose one or more models
-TUNING_FAST = True
 TUNING_EPOCHS = 10
 TUNING_STEPS_TRAIN = None
 TUNING_STEPS_VAL = None
